9886172008e7eed742038c69123160f1b1e293f7
hanar <hanar@zillowgroup.com>
2019-07-15 11:49:20 -0700

    Added backend folders

---
+++ src/server/routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+    router = express.Router(),
+    users = require('../models/user');
+mongoose = require('mongoose');
+var async = require("async");
+
+
+
+//at the end of page
+module.exports = router ;
---
+++ src/server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const os = require('os');
+
+var router = express.Router(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+const app = express();
+
+var port = process.env.PORT || 4000;
+
+// Connect to a MongoDB
+mongoose.connect("mongodb+srv://hana:<123>@cluster0-75gqk.mongodb.net/test?retryWrites=true&w=majority",  { useNewUrlParser: true })
+.then(() => console.log("Connection Successful"))
+.catch(err => console.log(err));
+
+// Allow CORS so that backend and frontend could be put on different servers
+var allowCrossDomain = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+};
+app.use(allowCrossDomain);
+
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+// Use routes as a module (see index.js)
+require('./routes')(app, router);
+
+// Start the server
+app.listen(port);
+console.log('Server running on port ' + port);
