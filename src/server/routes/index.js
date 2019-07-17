e3dbe32b37a0ee158fa89a1b01f4a31a157da340
oluwanifemia <nifemiadeyemo@gmail.com>
2019-07-17 16:27:14 -0700

    merge-with-master

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,4 +1,4 @@
-// EXAMPLE
+
 /*
  * Connect all of your endpoints together here.
  */
@@ -6,10 +6,11 @@ const dog = require('./dog.js');
 const owner = require('./owners.js');
 const schedule = require('./schedule.js');
 const login = require('./login');
+const location = require('./location.js');
 module.exports = (app, router) => {
     app.use('/dogs', dog);
     app.use('/owners', owner);
     app.use('/schedules', schedule);
-    app.use('/login', login);
+    app.use('/login', login);  
+    app.use('/location', location);
 };
-
