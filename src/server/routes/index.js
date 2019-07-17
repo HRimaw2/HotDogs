d94b69228106e70acd03afc1fa67087e12970219
oluwanifemia <nifemiadeyemo@gmail.com>
2019-07-17 16:20:17 -0700

    created-login-endpoint

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,9 +5,11 @@
 const dog = require('./dog.js');
 const owner = require('./owners.js');
 const schedule = require('./schedule.js');
+const login = require('./login');
 module.exports = (app, router) => {
     app.use('/dogs', dog);
     app.use('/owners', owner);
-    app.use('/schedules', schedule)
+    app.use('/schedules', schedule);
+    app.use('/login', login);
 };
 
