6bc6a443a86479ed9798407dcf1dfafea81e8b6e
yilina <yilina@hotpads.com>
2019-07-17 11:08:31 -0700

    Add searchFilters.js

---
+++ src/client/components/SearchFilters.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../styles/app.css';
+
+class SearchFilters extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Find me</p>
+            </div>
+        );
+    }
+}
+
+export default SearchFilters;
