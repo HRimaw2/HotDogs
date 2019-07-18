887951d76b4f5abb6651be43e6680627e787349c
atearjen <amtearjen@ucdavis.edu>
2019-07-18 13:50:38 -0700

    yikes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,14 +5,6 @@
 const dog = require('./dog.js');
 const owner = require('./owners.js');
 const schedule = require('./schedule.js');
-<<<<<<< HEAD
-const location = require('./location.js');
-module.exports = (app, router) => {
-    app.use('/dogs', dog);
-    app.use('/owners', owner);
-    app.use('/schedules', schedule)
-    app.use('/location', location)
-=======
 const login = require('./login');
 const location = require('./location.js');
 
@@ -20,7 +12,7 @@ module.exports = (app, router) => {
     app.use('/dogs', dog);
     app.use('/owners', owner);
     app.use('/schedules', schedule);
-    app.use('/login', login);  
+    app.use('/login', login);
     app.use('/location', location);
->>>>>>> 5da5347c7d29b05d7e9666d29af780e02e2cb391
+
 };
