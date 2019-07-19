2f208e2b1289d6735dea362f698472948800777e
hanar <hanar@zillowgroup.com>
2019-07-18 22:31:53 -0700

    made frontend login for login

--- src/client/components/LoginRegInputs.js
+++ src/client/components/LoginRegInputs.js
@@ -6,9 +6,10 @@ class LoginRegInputs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      type: this.props.type,
       email: '',
-      password: ''
+      password: '',
+      type: this.props.type,
+      login: this.props.login
     };
   }
 
@@ -19,16 +20,46 @@ class LoginRegInputs extends Component {
     this.setState({ password: e.target.value });
   };
 
+  componentDidMount(){
+    if (this.props.type==="login")
+    this.setState({login:true})
+  }
+
+  makeReg=()=>{
+    this.setState({login:false})
+    this.setState({type:"Register"})
+  }
+
+  makeLog=()=>{
+    this.setState({login:true})
+    this.setState({type:"Login"})
+  }
+
+  onSubmit =()=> {
+    
+  }
+
   render() {
     return (
       <div className="spaced">
+        <div>{this.state.type}</div>
         <Col className="user">Username: <input onChange={this.handleEmailChange}
                                                className="login-text" type="text"
                                                placeholder='username'/> </Col>
         <Col className="pass">Password: <input onChange={this.handlePasswordChange}
                                                className="login-text" type="password"
                                                placeholder='password'/></Col>
-        <Row className="loginButton"><Button>{this.state.type}</Button></Row>
+        <Row className="loginButton">
+          <Button>{this.state.type}</Button>
+        </Row>
+        <Row>
+            {
+              this.state.login ?
+                <Button onClick={this.makeReg} >Register Instead</Button>
+              :
+              <Button onClick={this.makeLog}>Login Instead</Button>
+            }
+          </Row>
       </div>
     );
   }
